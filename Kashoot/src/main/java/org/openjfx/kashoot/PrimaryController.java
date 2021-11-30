c5185bf22bee759205a62e911da4226a7f53edfd
EdwardR7 <36159443+EdwardR7@users.noreply.github.com>
2021-12-01 00:42:51 +0100

    Update PrimaryController.java

--- Kashoot/src/main/java/org/openjfx/kashoot/PrimaryController.java
+++ Kashoot/src/main/java/org/openjfx/kashoot/PrimaryController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 
 public class PrimaryController {
 
+    //FXML
+    
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
